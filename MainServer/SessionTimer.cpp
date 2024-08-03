5b5e752b3cad3c050c42d0d56e8c90dc1a7049d7
binlee0903 <egb3543@gmail.com>
2024-08-03 18:45:48 +0900

    fix: fix pch error

--- MainServer/SessionTimer.cpp
+++ MainServer/SessionTimer.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "SessionTimer.h"
 
 SessionTimer::SessionTimer()
--- MainServer/SessionTimer.h
+++ MainServer/SessionTimer.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <chrono>
-#include <cstdint>
-
 #ifndef SESSION_TIMER_GUARD
 #define SESSION_TIMER_GUARD
 
